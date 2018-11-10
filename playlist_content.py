1aeca01484784718a9c73045ee9473a515db2400
Humayun Khan <humayun.khan@mail.mcgill.ca>
2018-11-11 03:44:55 +0500

    soundcloud playlist script functional

---
+++ playlist_content.py
@@ -0,0 +1,29 @@
+#Deps: pip install soundcloud
+import soundcloud
+import json
+client = soundcloud.Client(client_id='YOUR_CLIENT_ID')
+
+link = input("enter playlist url from soundcloud ")
+res = client.get('/resolve', url=link)
+
+playlist = client.get("/playlists/"+str(res.id)+"/")
+
+print("Title of your playlist")
+print(playlist.title)
+print("\n")
+yo =  json.dumps(playlist.tracks)
+aye = json.loads(yo)
+client = soundcloud.Client(client_id=CLIENT_ID)
+
+#Playlist id =150058341 for mann ki baat sepetember 2015
+#playlist = client.get('/playlists/150058341/')
+
+print(playlist.title)
+yo =  json.dumps(playlist.tracks)
+aye = json.loads(yo)
+i = 0
+
+#method of printing from json data is a bit hacky
+print("Playlist content:")
+for i in xrange(yo.count('bpm')):
+	print((i,aye[i]["title"],aye[i]["id"]))
