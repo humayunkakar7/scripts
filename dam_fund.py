3b9f7a9e7c535fb76138b333387b6988ed6a5213
Humayun Khan <humayun.khan@mail.mcgill.ca>
2019-01-06 13:15:52 -0500

    adding chat analyzer

--- dam_fund.py
+++ dam_fund.py
@@ -7,8 +7,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import re
+import seaborn as sns
+
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
